01829e779415def21d1bbca3fa181c71b5298c5c
Zusita <118792001+Zozuke@users.noreply.github.com>
2025-11-05 14:27:55 -0600

    Add files via upload

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Servidor funcionando correctamente 🚀");
+});
+
+app.listen(3000, () => {
+  console.log("Servidor ejecutándose en http://localhost:3000");
+});
